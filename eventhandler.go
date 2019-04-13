66cb09eb65c11f29d27f5c9d337b064680df1ddd
Martin Fylke <martin.fylke@volvocars.com>
2019-04-13 20:24:31 +0200

    Returning error and number of events being emitted

--- eventhandler.go
+++ eventhandler.go
@@ -1,5 +1,10 @@
 package gull
 
+import (
+	"errors"
+	"fmt"
+)
+
 type event struct {
 	s string
 	f func()
@@ -9,7 +14,7 @@ type fn func()
 
 var eventHandlers = []event{}
 
-func EmitEvent(s string) {
+func EmitEvent(s string) (int, error){
 	var selectedEvents = []fn{}
 	for _, event := range eventHandlers{
 		if event.s == s {
@@ -17,9 +22,15 @@ func EmitEvent(s string) {
 		}
 	}
 
+	if len(selectedEvents) <= 0 {
+		return 0, errors.New(fmt.Sprintf("No event registered for: %s", s) )
+	}
+
 	for _, fn := range selectedEvents {
 		go fn()
 	}
+
+	return len(selectedEvents), nil
 }
 
 func Subscribe(s string, f fn) {
--- eventhandler_test.go
+++ eventhandler_test.go
@@ -1,20 +1,53 @@
 package gull
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
+var a = 0
+var b = 0
 
 func TestEmitEvent(t *testing.T) {
-	Subscribe("test", Happen)
-	Subscribe("test", Happen2)
-	EmitEvent("test")
+	Subscribe("test", happen)
+	Subscribe("test", happen2)
+	_, _ = EmitEvent("test")
+
+	time.Sleep(10 * time.Millisecond)
+
+	aGot := a
+	bGot := b
+	if aGot != 1 {
+		t.Errorf("a = %d, want 1", aGot)
+	}
+
+	if bGot != 2 {
+		t.Errorf("b = %d, want 2", bGot)
+	}
 }
 
-func Happen() {
-	a := 1
-	a = a+2
+func TestEmitEventCount(t *testing.T) {
+	Subscribe("testcount", happen)
+	Subscribe("testcount", happen2)
+	i, _ := EmitEvent("testcount")
+
+	if i != 2 {
+		t.Errorf("Should been 2, was: %d", i)
+	}
 }
 
-func Happen2() {
-	a := 1
-	a = a+2
+func TestEmitEventError(t *testing.T) {
+	_, e := EmitEvent("noevent")
+
+	if e == nil {
+		t.Error("Should return error, didn't")
+	}
+}
+
+func happen() {
+	a = 1
+}
+
+func happen2() {
+	b = 2
 }
